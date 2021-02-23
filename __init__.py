08e2e98a7ff2890fac442da9474a8aba0d3ecb10
shudeng <wsd19@tsinghua.edu.cn>
2021-02-23 19:33:21 +0800

    universal_init

--- __init__.py
+++ __init__.py
@@ -23,6 +23,15 @@ class Attacker(BatchAttack):
         else:
             self.num_classes=10
 
+
+        self.universal_perturbation_ph = tf.placeholder(self.model.x_dtype, self.xs_ph.shape[1:])
+        self.universal_perturbation = tf.Variable(tf.zeros(shape=self.xs_ph.shape[1:], dtype=self.model.x_dtype))
+        self.setup_universal_perturbation = [self.xs_var.assign(self.xs_ph),
+                self.ys_var.assign(self.ys_ph), 
+                self.universal_perturbation.assign(self.universal_perturbation_ph)]
+        self.grad_perturbation = self._get_gradient_universal_perturbation()
+
+
         self.tf_w_ph = tf.placeholder(self.model.x_dtype, (batch_size, self.num_classes))
         self.tf_w = tf.Variable(tf.zeros(shape=(batch_size, self.num_classes), dtype=self.model.x_dtype))
 
@@ -34,6 +43,8 @@ class Attacker(BatchAttack):
         self.grad_cw, self.loss_cw, self.stop_mask_cw = self._get_gradients(loss_type="cw")
 
         self.iteration = 80
+
+        self.perturbations=None
 
     def config(self, **kwargs):
         if 'magnitude' in kwargs:
@@ -55,23 +66,44 @@ class Attacker(BatchAttack):
             second_scores = tf.reduce_max((1- mask) * logits,  axis=1)
             loss = -(label_score - second_scores)
 
+            loss += tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.ys_var, logits=logits)
+
         grad = tf.gradients(loss, self.xs_var)[0]
         stop_mask = tf.cast(tf.equal(label, self.ys_var), dtype=tf.float32)
         return grad, loss, stop_mask
+
+    def _get_gradient_universal_perturbation(self):
+        logits, label = self.model._logits_and_labels(self.xs_var+self.universal_perturbation[None, :,:,:])
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.ys_var, logits=logits)
+        grad = tf.gradients(loss, self.universal_perturbation)[0]
+        return grad
+
 
 
 
     def batch_attack(self, xs, ys=None, ys_target=None):
         xs_lo, xs_hi = xs - self.eps, xs + self.eps
-        xs_adv = xs
 
         prev_grad = np.zeros(xs.shape)
         stop_mask = None
 
+        if self.perturbations is None:
+            print("self.perturbations is None")
+            self.perturbations = np.zeros(xs.shape[1:])
+
+        for i in range(10):
+            self._session.run(self.setup_universal_perturbation,  feed_dict={self.xs_ph: xs, self.ys_ph: ys,
+                self.universal_perturbation_ph:self.perturbations})
+            grad = self._session.run(self.grad_perturbation)
+            self.perturbations = self.perturbations+np.sign(grad)
+            self.perturbations = np.clip(self.perturbations, -self.eps, self.eps)
+
+        xs_adv = xs + self.perturbations[None, :, :, :] 
         for i in range(self.iteration):
             #if i%20==0 or i%20==1:
-            if i%25<3:
-                if i%25==0:
+            #if i%80<3:
+            if False:
+                if i%80==0:
                     self._session.run(self.setup_tf_w, feed_dict={self.tf_w_ph: 2*np.random.uniform(size=(self.batch_size, self.num_classes))-1})
                 if stop_mask is not None:
                     xs_adv = xs_adv * (1-stop_mask[:, None, None, None]) + (xs+self.init_delta()) * (stop_mask[:, None, None, None])
@@ -80,9 +112,9 @@ class Attacker(BatchAttack):
                 grad = self._session.run(self.grad_ods)
                 loss, stop_mask = self.loss_ods, self.stop_mask_ods
             else: 
-                osd_flag = False
                 self._session.run(self.setup,  feed_dict={self.xs_ph: xs_adv, self.ys_ph: ys})
                 grad = self._session.run(self.grad_cw)
+                #grad = self._session.run(self.grad_ce)
                 loss, stop_mask = self.loss_ce, self.stop_mask_ce
 
             grad = grad.reshape(self.batch_size, *self.model.x_shape)
